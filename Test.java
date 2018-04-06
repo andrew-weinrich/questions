1fc41c13c04cca973f512a748216dfc7b950b09c
Andrew Weinrich <andrew.weinrich@gmail.com>
2018-04-06 15:29:16 -0500

    Added tests for question 1

--- Test.java
+++ Test.java
@@ -5,20 +5,64 @@ public class Test {
     private static Solution solution = new Solution();
     
     public static void main(String[] args) {
-        TestSum1();
-        TestSum2();
-        TestSum3();
+        System.out.println("Testing testSum()");
+        testSum1();
+        testSum2();
+        testSum3();
         
-        TestMerge1();
-        TestMerge2();
-        TestMerge3();
-        TestMerge4();
-        
-        TestAdd1();
-        TestAdd2();
-        TestAdd3();
+        //testMerge1();
+        //testMerge2();
+        //testMerge3();
+        //testMerge4();
+        //
+        //testAdd1();
+        //testAdd2();
+        //testAdd3();
     }
     
+    
+    private static void testSum(int testId, int[] nums, int target, int[] expectedSolution) {
+        try {
+            int[] actualSolution = solution.twoSum(nums, target);
+        
+            if (actualSolution.length != 2) {
+                throw new Exception("Invalid solution length: " + actualSolution.length);
+            }
+        
+            if (actualSolution[0] != expectedSolution[0]) {
+                throw new Exception("Invalid first element: " + actualSolution[0] + "; expected " + expectedSolution[0]);
+            }
+        
+            if (actualSolution[1] != expectedSolution[1]) {
+                throw new Exception("Invalid second element: " + actualSolution[1] + "; expected " + expectedSolution[1]);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in testSum " + testId + ": " + e.getMessage());
+        }
+    }
+    
+    
+    private static void testSum1() {
+        testSum(1,
+                new int[]{ 2, 7, 11, 15 },
+                9,
+                new int[]{ 0, 1 });
+    }
+    
+    private static void testSum2() {
+        testSum(2,
+                new int[]{ 3, 7, 1, 10 },
+                8,
+                new int[]{ 1, 2 });
+    }
+    
+    private static void testSum3() {
+        testSum(3,
+                new int[]{ 2, 2 },
+                4,
+                new int[]{ 0, 1 });
+    }
     
     
 }
